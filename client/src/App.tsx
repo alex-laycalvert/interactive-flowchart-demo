93942f535d9e45c7b1a0a59153a86b18641de4e5
Alexander Lay-Calvert <alex.laycalvert@gmail.com>
2023-03-18 18:38:06 -0400

    Formatting

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -1,4 +1,11 @@
-import { FC, useEffect, useRef } from "react";
+import {
+    FC,
+    useEffect,
+    useRef,
+    useState,
+    DragEvent,
+    DragEventHandler,
+} from "react";
 import ReactFlow, {
     ReactFlowProvider,
     NodeChange,
@@ -11,15 +18,20 @@ import ReactFlow, {
     applyEdgeChanges,
     addEdge,
     ConnectionLineType,
+    ReactFlowInstance,
 } from "reactflow";
 import { WebrtcProvider } from "y-webrtc";
 import * as Y from "yjs";
+import { v4 as uuid } from "uuid";
 
 import "reactflow/dist/style.css";
 
 const App: FC = () => {
     const doc = useRef<Y.Doc>(new Y.Doc());
     const data = useRef<Y.Map<any>>(doc.current.getMap());
+    const reactflow = useRef<HTMLDivElement>(null);
+    const [reactFlowInstance, setReactFlowInstance] =
+        useState<ReactFlowInstance | null>(null);
     const [nodes, setNodes] = useNodesState([]);
     const [edges, setEdges] = useEdgesState([]);
 
@@ -28,7 +40,6 @@ const App: FC = () => {
     };
 
     const onEdgesChange = (changes: EdgeChange[]) => {
-        console.log("change");
         data.current.set("edges", applyEdgeChanges(changes, edges));
     };
 
@@ -46,6 +57,46 @@ const App: FC = () => {
         );
     };
 
+    const onDragStart = (
+        event: DragEvent<HTMLDivElement>,
+        nodeType: string
+    ) => {
+        if (!event.dataTransfer) {
+            return;
+        }
+        event.dataTransfer.setData("application/reactflow", nodeType);
+        event.dataTransfer.effectAllowed = "move";
+    };
+
+    const onDragOver: DragEventHandler<HTMLDivElement> = (event) => {
+        event.preventDefault();
+        event.dataTransfer.dropEffect = "move";
+    };
+
+    const onDrop: DragEventHandler<HTMLDivElement> = (event) => {
+        event.preventDefault();
+        if (!reactflow.current || !reactFlowInstance) {
+            return;
+        }
+        const reactFlowBounds = reactflow.current.getBoundingClientRect();
+        const type = event.dataTransfer.getData("application/reactflow");
+        // check if the dropped element is valid
+        if (typeof type === "undefined" || !type) {
+            return;
+        }
+        const position = reactFlowInstance.project({
+            x: event.clientX - reactFlowBounds.left,
+            y: event.clientY - reactFlowBounds.top,
+        });
+        const newNode = {
+            id: uuid(),
+            type,
+            position,
+            data: { label: "Node" },
+        };
+        data.current.set("nodes", nodes.concat(newNode));
+    };
+
     const save = async () => {
         try {
             const body = JSON.stringify({ nodes, edges });
@@ -56,7 +107,7 @@ const App: FC = () => {
                 },
                 body,
             });
-            console.log(await response.json());
+            console.info(await response.json());
         } catch (e) {
             console.error(e);
         }
@@ -89,28 +140,75 @@ const App: FC = () => {
             if (!existing) {
                 await fetchData();
             }
-            setNodes(data.current.get("nodes"));
-            setEdges(data.current.get("edges"));
+            const initialNodes = data.current.get("nodes");
+            if (initialNodes?.length > 0) {
+                setNodes(initialNodes);
+            }
+            const initialEdges = data.current.get("edges");
+            if (initialEdges?.length > 0) {
+                setEdges(initialEdges);
+            }
             doc.current.getMap().observeDeep(() => {
-                setNodes(data.current.get("nodes"));
-                setEdges(data.current.get("edges"));
+                const initialNodes = data.current.get("nodes");
+                if (initialNodes?.length > 0) {
+                    setNodes(initialNodes);
+                }
+                const initialEdges = data.current.get("edges");
+                if (initialEdges?.length > 0) {
+                    setEdges(initialEdges);
+                }
             });
         })();
     }, [doc]);
 
     return (
         <ReactFlowProvider>
-            <div style={{ width: "100%", height: "100vh" }}>
-                <button style={{}} onClick={save}>
+            <div style={{ width: "100%", height: "98vh" }} ref={reactflow}>
+                <button style={{ position: "fixed", zIndex: 5 }} onClick={save}>
                     Save Changes
                 </button>
+                <div
+                    style={{
+                        position: "absolute",
+                        top: 5,
+                        right: 20,
+                        zIndex: 5,
+                    }}
+                >
+                    <div
+                        style={{
+                            width: "100%",
+                            textAlign: "center",
+                        }}
+                    >
+                        Drag Some Nodes Over!
+                    </div>
+                    <br />
+                    <div
+                        onDragStart={(event) => onDragStart(event, "default")}
+                        style={{
+                            width: "100%",
+                            padding: "5px",
+                            border: "1px solid black",
+                            borderRadius: "5px",
+                            textAlign: "center",
+                        }}
+                        draggable
+                    >
+                        Node
+                    </div>
+                </div>
                 <ReactFlow
                     nodes={nodes}
                     edges={edges}
                     onNodesChange={onNodesChange}
                     onEdgesChange={onEdgesChange}
                     onConnect={onConnect}
+                    onInit={setReactFlowInstance}
+                    onDrop={onDrop}
+                    onDragOver={onDragOver}
                     minZoom={0.1}
+                    fitView
                 >
                     <Controls />
                     <Background />
